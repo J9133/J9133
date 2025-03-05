2e14c6078ee74ecbe10b8dea1323377590a94685
J9133 <jad.salim.13.2011@gmail.com>
2025-03-06 01:03:20 +0200

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,20 +1,20 @@
 async function checkAppStatus() {
-            try {
-                const response = await fetch('https://raw.githubusercontent.com/JAD506/JAD506/main/config.json?timestamp=' + new Date().getTime());
-                if (!response.ok) {
-                    throw new Error("HTTP error! status: ${response.status}");
-                }
-                const config = await response.json();
-                console.log(config.enabled)
-                if (config.enabled) {
-                    window.location.href = "https://www.youtube.com";
-                } else {
-                    document.body.innerHTML = "<h1>التطبيق معطل حاليًا</h1>";
-                }
-            } catch (error) {
-                document.body.innerHTML = "<h1>حدث خطأ في الاتصال</h1>";
-            }
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/JAD506/JAD506/main/config.json?timestamp=' + new Date().getTime());
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);  // تم تعديل هنا
         }
+        const config = await response.json();
+        console.log(config.enabled)
+        if (config.enabled) {
+            window.location.href = "https://www.youtube.com";
+        } else {
+            document.body.innerHTML = "<h1>التطبيق معطل حاليًا</h1>";
+        }
+    } catch (error) {
+        document.body.innerHTML = "<h1>حدث خطأ في الاتصال</h1>";
+    }
+}
 
-        setInterval(checkAppStatus, 1000);
-        window.onload = checkAppStatus;
+setInterval(checkAppStatus, 1000);
+window.onload = checkAppStatus;
